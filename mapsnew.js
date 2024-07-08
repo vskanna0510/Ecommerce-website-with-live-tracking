b38aacccb346ef70f33340e6d78e84ed1586ade4
vskanna0510 <vskanna2003@gmail.com>
2024-07-08 08:13:21 +0530

    first commit

---
+++ mapsnew.js
@@ -0,0 +1,35 @@
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=myMap"></script>
+
+var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 15,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+var marker = null;
+
+function autoUpdate() {
+  navigator.geolocation.getCurrentPosition(function(position) {  
+    var newPoint = new google.maps.LatLng(position.coords.latitude, 
+                                          position.coords.longitude);
+
+    if (marker) {
+      // Marker already created - Move it
+      marker.setPosition(newPoint);
+    }
+    else {
+      // Marker does not exist - Create it
+      marker = new google.maps.Marker({
+        position: newPoint,
+        map: map
+      });
+    }
+
+    // Center the map on the new position
+    map.setCenter(newPoint);
+  }); 
+
+  // Call the autoUpdate() function every 5 seconds
+  setTimeout(autoUpdate, 5000);
+}
+
+autoUpdate();
